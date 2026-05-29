3f03738c3ff644339837f0f182602d456bd94339
MessaoudiIshak <ishakabderrahmanem@gmail.com>
2026-05-29 16:47:15 +0200

    fix: update eslint config to allow node process global

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,17 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.node // 👈 THIS LINE RIGHT HERE tells ESLint that 'process' is totally fine!
+      } 
+    } 
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/__tests__/**/*.js", "**/*.test.js"], languageOptions: { globals: globals.jest } },
 ]);
